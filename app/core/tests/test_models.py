4ba818c395ca504185d17825296c5a5d560a389f
Hardik <hardikzurdo>
2019-05-03 20:12:22 -0400

    user models

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTest(TestCase):
+
+    def test_create_user(self):
+        """test User with email address and password"""
+        email = 'testemail@gnail.com'
+        password = 'Testpass1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ test for email for exampt the uper lower cases"""
+        email = 'test@GNAIL.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test users has email ID"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating siperuser ans staff permission"""
+        user = get_user_model().objects.create_superuser(
+            'nano@tata.com',
+            'tatanano'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
